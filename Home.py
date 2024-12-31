71979c0e8eb3958dc1fbc60f7e7263ee0773d52c
Aryani Wido <73062483+aryaniww@users.noreply.github.com>
2024-12-31 17:05:28 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 pg = st.navigation([st.Page("Home.py", title="Home"),
                     st.Page("file-4-model/UAS_DS02_Aryani.py", title="Docs")])
-pg.run()
+# pg.run()
 st.title("Welcome")
 st.write("Insert your data here:")
