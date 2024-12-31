c01ec0e78b26ae51201f4bb40a88dccf1ec89ac3
Aryani Wido <73062483+aryaniww@users.noreply.github.com>
2024-12-31 17:57:52 +0700

    Update UAS_DS02_Aryani.py

--- file-4-model/UAS_DS02_Aryani.py
+++ file-4-model/UAS_DS02_Aryani.py
@@ -43,8 +43,8 @@ from sklearn import metrics
 import streamlit as st
 
 
-# pg = st.navigation([st.Page("coba_insert_streamlit.py", title="Home"),
-#                     st.Page("file-4-model/UAS_DS02_Aryani.py", title="Docs")]) 
+pg = st.navigation([st.Page("coba_insert_streamlit.py", title="Home"),
+                    st.Page("UAS_DS02_Aryani.py", title="Docs")]) 
 
 # from google.colab import drive
 # drive.mount('/content/drive')
