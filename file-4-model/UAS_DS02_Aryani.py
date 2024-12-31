7b3b1eaf224a9d7763175d7c5747c555447aebb0
Aryani Wido <73062483+aryaniww@users.noreply.github.com>
2024-12-31 17:30:18 +0700

    Update UAS_DS02_Aryani.py

--- file-4-model/UAS_DS02_Aryani.py
+++ file-4-model/UAS_DS02_Aryani.py
@@ -45,7 +45,7 @@ import streamlit as st
 
 pg = st.navigation([st.Page("coba_insert_streamlit.py", title="Home"),
                     st.Page("file-4-model/UAS_DS02_Aryani.py", title="Docs")]) 
-
+pg.run()
 # from google.colab import drive
 # drive.mount('/content/drive')
 # water_data = pd.read_csv('/content/drive/MyDrive/water_potability.csv')
